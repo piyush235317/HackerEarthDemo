fd73350a5bdc6e107bfbac89d5f28c8226a53ec9
Piyush <piyushkmr235317@gmail.com>
2026-09-21 16:09:35 +0530

    Update watermark email and timestamp

--- app/test/page.js
+++ app/test/page.js
@@ -402,8 +402,8 @@ export default function TestPage() {
                   <div className={styles.watermarkContainer}>
                     {[1, 2, 3, 4].map((i) => (
                       <div key={i} className={styles.watermarkItem}>
-                        <div>mde2025011@iiita.ac.in</div>
-                        <div>2026-09-21 19:12 • UTC</div>
+                        <div>anandbhansinghchouhan@gmail.com</div>
+                        <div>2026-09-21 19:11 • UTC</div>
                       </div>
                     ))}
                   </div>
@@ -510,7 +510,7 @@ export default function TestPage() {
                 <div className={styles.watermarkContainer}>
                   {[1, 2, 3, 4].map((i) => (
                     <div key={i} className={styles.watermarkItem}>
-                      <div>mde2025011@iiita.ac.in</div>
+                      <div>anandbhansinghchouhan@gmail.com</div>
                       <div>2026-09-20 12:13 • UTC</div>
                     </div>
                   ))}
